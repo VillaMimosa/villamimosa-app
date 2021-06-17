6c3e2b111fea8dd2d1b236bf30524a4d1c3bf521
AndreuSerraSastre <andreuserrasastre@kuarasoftware.com>
2021-06-17 17:57:40 +0200

    errores sin datos

--- src/components/DetailView/ObjectDetail.js
+++ src/components/DetailView/ObjectDetail.js
@@ -10,7 +10,7 @@ import { ESP } from "../../constants/idioms";
 import { URL } from "../../constants";
 import marked from "marked";
 
-const ObjectDetail = ({detalle}) => {
+const ObjectDetail = ({ detalle }) => {
 
     const currentIdiom = useSelector(getCurrentIdiom);
 
@@ -19,14 +19,16 @@ const ObjectDetail = ({detalle}) => {
     return (
         <ObjectDetailMain>
             <h1>{currentIdiom.value === ESP ? TituloESP : TituloENG}</h1>
-            <Divider>{currentIdiom.value === ESP ? 'Vídeo' : 'Video'}</Divider>
-            <Player
-                playsInline
-                poster="/assets/poster.png"
-                src={currentIdiom.value === ESP ? URL + VideoESP.url : URL + VideoENG.url}
-            />
-            <Divider>{currentIdiom.value === ESP ? 'Explicación' : 'Explanation'}</Divider>
-            <div dangerouslySetInnerHTML={{__html: marked(currentIdiom.value === ESP ? ParagrafosESP : ParagrafosENG)}} />
+            {VideoESP && VideoENG && <Divider>{currentIdiom.value === ESP ? 'Vídeo' : 'Video'}</Divider>}
+            {VideoESP && VideoENG &&
+                <Player
+                    playsInline
+                    poster="/assets/poster.png"
+                    src={currentIdiom.value === ESP ? URL + VideoESP.url : URL + VideoENG?.url}
+                />}
+            {ParagrafosESP && ParagrafosENG && <Divider>{currentIdiom.value === ESP ? 'Explicación' : 'Explanation'}</Divider>}
+            {ParagrafosESP && ParagrafosENG &&
+                <div dangerouslySetInnerHTML={{ __html: marked(currentIdiom.value === ESP ? ParagrafosESP : ParagrafosENG) }} />}
             <Divider></Divider>
         </ObjectDetailMain>
     )
